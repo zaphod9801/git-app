12f4666237a9b380a620853f806b67a0590f1d1b
zaphod9801 <71454879+zaphod9801@users.noreply.github.com>
2022-05-08 16:52:37 -0500

    Update ReposDetails.jsx

--- src/components/ReposDetails.jsx
+++ src/components/ReposDetails.jsx
@@ -6,11 +6,11 @@ import { RepoItem } from "./RepoItem";
 import { TableHead } from "./TableHead";
 
 export function ReposDetails() {
-  const [repos, setRepos] = useState([]);
+  const [repos, setRepos] = useState([]); //The necessary consts
   const location = useLocation();
   const { from } = location.state;
 
-  useEffect(() => {
+  useEffect(() => { //Change the page title and try to ask the Github API for the searched user repositories when the page load
     document.title = "GIt hub repos"
     API.getReposByUsername(from.login)
       .then((res) => setRepos(res)
@@ -22,7 +22,7 @@ export function ReposDetails() {
   return (
     <>
       <Center>
-        <Flex color='white'>
+        <Flex color='white'> //Flex to display ordered the searched user avatar and username
 
           <Box bg="gray.200" pr={8} pt={1} mt={3} borderRadius="lg">
             <Image m={3} pl={6} src={from.avatar_url} width={100} />
@@ -37,14 +37,14 @@ export function ReposDetails() {
       </Center>
       <Flex color='white'>
         <Box bg="gray.200" m={4} ml={3} borderRadius="lg">
-          <TableContainer >
+          <TableContainer > //Table containing all the searched user repositories
             <Table variant='striped' colorScheme='teal'>
               <TableCaption>User Repositories</TableCaption>
               <Thead>
                 <TableHead />
               </Thead>
               <Tbody>
-                {repos.map(repo => (
+                {repos.map(repo => ( //Mapping in the list of repositories, check RepoItem
                   <RepoItem key={repo.id} {...repo} />
                 ))}
               </Tbody>
