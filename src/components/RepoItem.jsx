bb0a3b3902ba5f9f897899572fbd6ffbdec56026
zaphod9801 <71454879+zaphod9801@users.noreply.github.com>
2022-05-08 16:56:22 -0500

    Update RepoItem.jsx

--- src/components/RepoItem.jsx
+++ src/components/RepoItem.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 
 
-export function RepoItem(repo) {
+export function RepoItem(repo) { //This function renders a single repository info with table elements in order to be used in the RepoDetails.jsx table
 
     return (
         <>
@@ -11,11 +11,11 @@ export function RepoItem(repo) {
                 <Th><Text fontSize="l" color="gray.500">  {repo.name} </Text></Th>
                 <Th><Text fontSize="l" color="gray.500">  {repo.language} </Text></Th>
                 <Th><Text fontSize="l" color="gray.500">  {repo.html_url} </Text></Th>
-                <Th><Text fontSize="l" color="gray.500">  {repo.updated_at ? repo.updated_at.split("T")[0] : null} </Text></Th>
+                <Th><Text fontSize="l" color="gray.500">  {repo.updated_at ? repo.updated_at.split("T")[0] : null} </Text></Th> //Treatment to the date
                 <Th>
                     <Text fontSize="l" color="gray.500">
                         {repo.tags_url} <Spacer />
-                        <a href={repo.tags_url} target="_blank" rel="noopener noreferrer" mb={2}>
+                        <a href={repo.tags_url} target="_blank" rel="noopener noreferrer" mb={2}> //External link to the API tag page
                             <Tag fontSize="md" bg="blue.200">
                                 see more
                             </Tag>
@@ -25,7 +25,7 @@ export function RepoItem(repo) {
                 <Th>
                     <Text fontSize="l" color="gray.500">
                         {repo.branches_url} <Spacer />
-                        <Link to={`/branches`} state={{ from: repo }} mb={2}>
+                        <Link to={`/branches`} state={{ from: repo }} mb={2}> //Internal link to the app branches view
                             <Tag fontSize="md" bg="blue.200">
                                 see more
                             </Tag>
